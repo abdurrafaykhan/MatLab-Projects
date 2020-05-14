27140e57df7f97061f199697293d6fa66e7c6c0b
Abdurrafay Khan <abdurrafay21000@gmail.com>
2020-05-13 20:13:00 -0400

    Added files for Lab 5

---
+++ Lab 5/Lab5.m
@@ -0,0 +1,12 @@
+for a=1:0.1:2
+    t=linspace(0,5,10001);
+    P=(1000/a).*exp(0.5*a*t);
+    plot(t,P);
+    hold on
+    grid on
+end
+legend('a=1.0','a=1.1','a=1.2','a=1.3','a=1.4','a=1.5','a=1.6','a=1.7','a=1.8','a=1.9','a=2.0');
+xlabel('Time (hours)');
+ylabel('Bacteria Population, P(t)');
+title('Bacteria Growth')
+
